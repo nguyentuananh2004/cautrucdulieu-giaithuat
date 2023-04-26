f1fc16a3931086a18669db9c4519ebf6d8e58291
nguyentuananh2004 <115202942+nguyentuananh2004@users.noreply.github.com>
2023-04-26 10:17:46 +0700

    Update Ước số chung lớn nhất.py

--- Ước số chung lớn nhất.py
+++ Ước số chung lớn nhất.py
@@ -1,8 +1,8 @@
 def USCLN(a, b):
-    if b == 0:
-        return a
+    if b == 0: 
+        return a #nếu b=0 thì USCLN là a
     else:
-        return USCLN(b, a % b)
+        return USCLN(b, a % b) #tiếp tục đệ quy và tìm USCLN của b và a % b (phần dư của a khi chia cho b). Với mỗi lần đệ quy, a sẽ được thay bằng b và b sẽ được thay bằng a % b, cho đến khi b bằng 0. Khi đó, USCLN(a, 0) = a, và a chính là kết quả của bài toán ban đầu.
 
 
 print(USCLN(12, 24))
