ddacc3936d44dafa5b2b352e0872380815d27817
nguyentuananh2004 <115202942+nguyentuananh2004@users.noreply.github.com>
2023-04-26 10:10:13 +0700

    Update Tính giai thừa của 1 số.py

--- Tính giai thừa của 1 số.py
+++ Tính giai thừa của 1 số.py
@@ -1,6 +1,6 @@
 def tinh_giai_thua(n):
     giai_thua = 1
-    for i in range(1, n+1):
-        giai_thua = giai_thua*i
+    for i in range(1, n+1): #vòng lặp i với phạm vi từ 1 đến n+1
+        giai_thua = giai_thua*i #giá trị giai thừa bằng giá trị giai thừa ban đầu nhân với i
     return giai_thua
 print(tinh_giai_thua(4))
