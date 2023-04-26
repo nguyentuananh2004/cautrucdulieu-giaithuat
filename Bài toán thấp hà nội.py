48a4b0888ba22cde1bdaa066a18ff8d2376b11d3
nguyentuananh2004 <115202942+nguyentuananh2004@users.noreply.github.com>
2023-04-26 10:06:14 +0700

    Update Bài toán thấp hà nội.py

--- Bài toán thấp hà nội.py
+++ Bài toán thấp hà nội.py
@@ -6,9 +6,11 @@ def TowerOfHanoi(n, source, destination, auxiliary):
     if n == 1:
         print("Di chuyển Đĩa 1 từ cột nguồn", source, "đến cột nhận", destination) #chạy từ nguồn đến cột nhận đến hỗ trợ
         return
-    TowerOfHanoi(n - 1, source, auxiliary, destination) #chạy từ nguồn đến hỗ trợ đến cột nhận
+    TowerOfHanoi(n - 1, source, auxiliary, destination) #giảm 1 đơn vị
+                                                        #chạy từ nguồn đến hỗ trợ đến cột nhận
     print("Di chuyển Đĩa", n, "từ cột nguồn", source, "đến cột nhận", destination)
-    TowerOfHanoi(n - 1, auxiliary, destination, source) #chạy từ hỗ trợ đến cột nhận đến nguồn
+    TowerOfHanoi(n - 1, auxiliary, destination, source) #giảm 1 đơn vị
+                                                        #chạy từ hỗ trợ đến cột nhận đến nguồn
 
 
 n = 4
